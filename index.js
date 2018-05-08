d6e469bca05fb30116a445aa8677c4f9b5c111ef
Jennifer Dobak <jdobak1@gmail.com>
2018-05-08 19:53:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 function produceDrivingRange(blockRange){
   return function(start, end) {
+    let miles = parseInt(start) - parseInt(end)
     let span = Math.abs(blockRange - miles)
     return miles < blockRange ? `within range by ${span}` : `${span} blocks out of range`
     }
